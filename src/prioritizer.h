d0c592d9479751099a86171dd041ba73d5a6fd4a
André Nusser <nusser@fmi.uni-stuttgart.de>
2015-05-31 15:19:48 +0200

    Redefined interface of Prioritizer.

--- src/prioritizer.h
+++ src/prioritizer.h
@@ -2,9 +2,7 @@
 
 #include "defs.h"
 #include "nodes_and_edges.h"
-#include "chgraph.h"
 
-#include <list>
 #include <vector>
 
 namespace chc
@@ -13,7 +11,7 @@ namespace chc
 /*
  * Interface of a Prioritizer used in the CHConstructor.
  */
-template <template <typename, typename...> class Container = std::list>
+template <template <typename, typename...> class Container>
 class Prioritizer
 {
 	protected:
@@ -22,90 +20,26 @@ class Prioritizer
 
 	public:
 		/*
-		 * Initialize the prioritizer list with <node_id_list>.
+		 * Initialize the prioritizer list with <node_ids>.
+		 */
+		virtual void init(container_type& node_ids) = 0;
+
+		/*
+		 * Removes <node_ids> from the list of nodes to be prioritized.
+		 */
+		virtual void remove(std::vector<bool> to_remove) = 0;
+
+		/*
+		 * Returns vector of nodes containing the ones to be contracted next.
 		 *
-		 * ATTENTION: Steals the content of nodes!
+		 * ATTENTION: The returned vector of nodes has to contain an independent set!
 		 */
-		void init(container_type& node_id_list);
+		virtual std::vector<NodeID> getNextNodes() = 0;
 
 		/*
-		 * Removes <node_id_list> from the list of nodes to be prioritized.
-		 * This is a generic remove. It might be faster to implement
-		 * ones own remove function in a derived class.
+		 * True iff there are still nodes left to contract.
 		 */
-		virtual void remove(std::vector<bool> to_remove);
-
-		/*
-		 * Returns list of nodes in (descending) order of their priority.
-		 */
-		container_type const& getPriorityList();
-
-		/*
-		 * Reevaluate the priorities of the nodes and update the list accordingly.
-		 * This function uses order_by_prio to achieve that.
-		 */
-		void update();
-
-		/*
-		 * Order <node_id_list> by priorities (descending).
-		 */
-		virtual void order_by_prio(container_type& node_id_list) = 0;
+		virtual bool hasNodesLeft() = 0;
 };
-
-template <template <typename, typename...> class Container>
-void Prioritizer<Container>::init(container_type& node_id_list)
-{
-	_prio_list = std::move(node_id_list);
-}
-
-template <template <typename, typename...> class Container>
-void Prioritizer<Container>::remove(std::vector<bool> to_remove)
-{
-	auto it(_prio_list.begin());
-	while (it != _prio_list.end()) {
-		if (to_remove[*it]) {
-			it = _prio_list.erase(it);
-		}
-		else {
-			it++;
-		}
-	}
-}
-
-template <template <typename, typename...> class Container>
-auto Prioritizer<Container>::getPriorityList() -> container_type const& 
-{
-	return _prio_list;
-}
-
-template <template <typename, typename...> class Container>
-void Prioritizer<Container>::update()
-{
-	order_by_prio(_prio_list);
-}
-
-/*
- * Example of usage of the Prioritizer Interface.
- */
-template <typename NodeT, typename EdgeT>
-class My_Prioritizer : public Prioritizer<std::list>
-{
-	private:
-		typedef My_Prioritizer::container_type container_type;
-		typedef CHGraph<NodeT, EdgeT> CHGraphT;
-
-		CHGraphT const& _base_graph;
-
-	public:
-		My_Prioritizer(CHGraphT const& base_graph) : _base_graph(base_graph) { }
-		void order_by_prio(container_type& node_id_list);
-};
-
-template <typename NodeT, typename EdgeT>
-void My_Prioritizer<NodeT, EdgeT>::order_by_prio(container_type& node_id_list)
-{
-	(void) node_id_list;
-	// TODO
-}
 
 }
